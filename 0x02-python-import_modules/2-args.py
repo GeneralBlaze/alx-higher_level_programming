65cee1d8d47c28ce1669e8674166a8684ae0283a
GeneralBlaze <benjaminprincewillgmail.com>
2023-01-13 04:31:34 +0000

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,8 @@ if __name__ == "__main__":
     arg_length = len(sys.argv)
     if arg_length == 1:
         print("{} arguments.".format(arg_length - 1))
-    elif arg_length == 2:
-        print("{} argument:".format(arg_length - 1))
+    elif arg_length > 1:
+	    for i in range(1,arg_length):
+		    print("{}: {}".format(i,sys.argv[i]))
     else:
         print("{} arguments:".format(arg_length - 1))
-
-    for i in range(1, arg_length):
-        print("{}: {}".format(i, sys.argv[i])
