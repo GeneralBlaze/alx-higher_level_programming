ed9c0d16610ed889f6c9f29355f7a8b436350aff
GeneralBlaze <benjaminprincewillgmail.com>
2023-01-14 03:11:30 +0000

    0-task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
-quare_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-
-    for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-
-    return (new_matrix)
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = [[i * i for i in row] for row in matrix]
+    return new
