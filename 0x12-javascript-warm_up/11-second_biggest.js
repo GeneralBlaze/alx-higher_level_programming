d5acbdfac4f7ef91824e045b40791fa59d8af8e0
GeneralBlaze <benjaminprincewill@gmail.com>
2023-04-12 15:00:47 -0700

    update

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function second_largest (num) {
+  const max = Math.max(...num);
+  let min = Math.min(...num);
+  for (const i of num) {
+    if (i > min && i < max) {
+      min = i;
+    }
+  }
+  return min;
+}
+const args = process.argv.slice(2);
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
+} else {
+  console.log(second(args));
+}
