b0a489a391b204f70985e203ba2d21434c6db607
GeneralBlaze <benjaminprincewill@gmail.com>
2023-04-17 12:04:31 -0700

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,15 +7,19 @@ import sys
 
 
 if __name__ == "__main__"
-    databaseConn = MySQLdb.connect(host="localhost",user=sys.argv[1],
-            port=3306,passwd=sys.argv[2],
-            db=sys.argv[3]
-            )
-    objCursor = databaseconn.cursor()
+
+    dbCon = MySQLdb.connect(
+                host="localhost",
+                user=sys.argv[1],
+                port=3306,
+                passwd=sys.argv[2],
+                db=sys.argv[3])
+
+    objCursor = dbCon.cursor()
 
     objCursor.execute("SELECT * FROM states")
     dispRows = objCursor.fetchall()
     for row in dispRows:
         print(row)
     objCursor.close()
-    databaseconn.close()
+    dbCon.close()
