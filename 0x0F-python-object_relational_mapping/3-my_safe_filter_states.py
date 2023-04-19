0e411a8b8b75a05e7143707394f2b17e6153fc06
GeneralBlaze <benjaminprincewill@gmail.com>
2023-04-18 22:28:34 -0700

    3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
+""" listing all states in a db"""
+
 import MySQLdb
 import sys
 
-if __name__ = "__main__":
+
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    stateName = sys.argv[4]
 
     dbCon = MySQLdb.connect(
                 host="localhost",
@@ -16,9 +20,9 @@ if __name__ = "__main__":
                 db=sys.argv[3])
 
     objCursor = dbCon.cursor()
+    sqlCmd = ("SELECT id, name FROM states WHERE name = %s ORDER BY id ASC")
 
-    objCursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-            ORDER BY states.id ASC".format(sys.argv[4])
+    objCursor.execute(sqlCmd, (state_name))
     dispRows = objCursor.fetchall()
     for row in dispRows:
         print(row)
