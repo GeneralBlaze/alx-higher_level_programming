780bac0d345062196700b3ead803e7560d1fc3ab
GeneralBlaze <benjaminprincewill@gmail.com>
2023-04-21 17:05:04 -0700

    redo

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
 
     dbCon = MySQLdb.connect(
                 host="localhost",
-                user=sys.argv[1],
                 port=3306,
-                passwd=sys.argv[2],
-                db=sys.argv[3])
+                user=sys.argv[1],
+                password=sys.argv[2],
+                database=sys.argv[3])
 
     objCursor = dbCon.cursor()
 
